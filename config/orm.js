e19318ac7c53af042c76e8cc6fec6dfe3b0a6167
calebwelch <caleb.welch11@gmail.com>
2020-06-24 19:08:09 -0400

    heroku set up. began controller logic

--- config/orm.js
+++ config/orm.js
@@ -28,3 +28,4 @@ let orm = {
     });
   },
 };
+module.exports = orm;
---
+++ controllers/burger_controller.js
@@ -0,0 +1,6 @@
+const orm = require("../config/orm.js");
+const express = require("express");
+let router = express.Router();
+// create routes
+
+router.get("/"(req,res)=>{})
