9896480ad42199a2a805bccf2612d12229745aab
calebwelch <caleb.welch11@gmail.com>
2020-06-24 14:52:54 -0400

    mysql jawbones

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+mysql = require("mysql");
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers",
+  });
+}
+
+connection.connect();
+module.exports = connection;
